734fbf0f904303486fd8454cf44f27a50c1965ac
mrwerner392 <mrwerner392@gmail.com>
2021-06-12 23:17:01 -0400

    User update action.

--- app/controllers/v1/users_controller.rb
+++ app/controllers/v1/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   skip_before_action :authorized, only: [:create]
-  before_action :get_user, only: [:show]
+  before_action :get_user, only: [:show, :update]
 
   def create
     username = params[:username]
@@ -18,10 +18,22 @@ class UsersController < ApplicationController
     render json: @user
   end
 
+  def update
+    if @user.update(user_params)
+      render json: @user
+    else
+      render json: { message: @user.errors.full_messages.first }, status: :unprocessible_entity
+    end
+  end
+
   private
 
   def get_user
     @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.permit(:first_name, :last_name, :username, :email)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   namespace :v1 do
-    resources :user, only: [:create, :show]
+    resources :user, only: [:create, :show, :update]
     resources :login, only: [:create]
   end
 
